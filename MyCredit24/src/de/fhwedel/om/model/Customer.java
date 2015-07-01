9bb9d15732c07669cdce92ee9fac67f35d183c31
ddekkers <daniel_dekkers@web.de>
2015-07-01 15:22:35 +0200

    jh - customer data object

--- MyCredit24/src/de/fhwedel/om/model/Customer.java
+++ MyCredit24/src/de/fhwedel/om/model/Customer.java
@@ -1,18 +1,12 @@
 package de.fhwedel.om.model;
 
-import java.util.LinkedList;
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
 
 @SuppressWarnings("serial")
 @Entity
@@ -23,82 +17,102 @@ import javax.persistence.OneToMany;
    )
 })
 public class Customer implements BusinessObject<Integer> {
-   
-   @Id
-   @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="CUST_SEQ")
-   @Column(name="customer_id")
-   private Integer customer_id;
-   
-   @Column(name="name")
-   private String prename;
-   
-   private String surname;
-   
-   private int age;
-   
-   @OneToMany(fetch=FetchType.EAGER, mappedBy="customer", cascade={CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REMOVE, CascadeType.REFRESH})
-   private List<Order> orders;
-   
-   public Customer() {
-      this("<neu>", "<neu>", 0);
-   }
 
-   public Customer(String prename, String surname, int age) {
-      this.prename = prename;
-      this.surname = surname;
-      this.age = age;
-      this.orders = new LinkedList<Order>();
-   }
-    
-   public String getPrename() {
-      return prename;
-   }
+	   @Id
+	   @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="CUST_SEQ")
+	   @Column(name="customer_id")
+	   private int customer_id;
+	   
+	   private int customer_number;
+	   
+	   private String surname;
+	   
+	   private String prename;
+	   
+	   private String street;
+	   
+	   private String postcode;
+	   
+	   private String city;
+	   
+	   public int getCustomerNumber() {
+		   return customer_number;
+	   }
+	   
+	   public void setCustomerNumber(int customer_number) {
+		   this.customer_number = customer_number;
+	   }
+	   
+	   public String getSurname() {
+		   return surname;
+	   }
 
-   public void setPrename(String prename) {
-      this.prename = prename;
-   }
+	   public void setSurname(String surname) {
+		   this.surname = surname;
+	   }
 
-   public String getSurname() {
-      return surname;
-   }
+	   public String getPrename() {
+	      return prename;
+	   }
 
-   public void setSurname(String surname) {
-      this.surname = surname;
-   }
+	   public void setName(String name) {
+	      this.prename = name;
+	   }
 
-   public int getAge() {
-      return age;
-   }
+	   public String getStreet() {
+		   return street;
+	   }
 
-   public void setAge(int age) {
-      this.age = age;
-   }
+	   public void setStreet(String street) {
+		   this.street = street;
+	   }
 
-   public List<Order> getOrders() {
-      return orders;
-   }
+	   public String getPostcode() {
+		   return postcode;
+	   }
 
-   public void setOrders(List<Order> orders) {
-      this.orders = orders;
-   }
-   
-   // Object-Methoden
+	   public void setPostcode(String postcode) {
+		   this.postcode = postcode;
+	   }
 
-   @Override
-   public String toString() {
-      return this.getCaption();
-   }
-   
-   // BusinessObject-Methoden 
-   
-   @Override
-   public Integer getID() {
-      return this.customer_id;
-   }
+	   public String getCity() {
+		   return city;
+	   }
 
-   @Override
-   public String getCaption() {
-      return "[Kunde " + this.getSurname() + ", Alter: " + this.getAge() + ", ID: " + this.getID() + "]";
-   }
-       
+	   public void setCity(String city) {
+		   this.city = city;
+	   }
+
+	   public Customer() {
+		   this(0, "", "", "", "", "");
+	   }
+	      
+	   public Customer(int debtor_number, String surname, String prename,  String street, String postcode, String city) {
+		   this.customer_number = debtor_number;
+		   this.surname = surname;
+		   this.prename = prename;
+		   this.street = street;
+		   this.postcode = postcode;
+		   this.city = city;
+	   }
+
+	   // Object-Methoden
+
+	   @Override
+	   public String toString() {
+	      return this.getCaption();
+	   }
+	   
+	   // BusinessObject-Methoden 
+	   
+	   @Override
+	   public Integer getID() {
+	      return this.customer_id;
+	   }
+
+	   @Override
+	   public String getCaption() {
+	      return "";
+	   }
+	   
 }
