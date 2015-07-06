b9a293b258ae45ed560fce7cbb2c2e7c71c2afb3
ddekkers <daniel_dekkers@web.de>
2015-07-06 22:23:56 +0200

    DD - Beispieldaten für Verträge mit Datum versehen

--- MyCredit24/src/de/fhwedel/om/server/OMServiceImpl.java
+++ MyCredit24/src/de/fhwedel/om/server/OMServiceImpl.java
@@ -122,9 +122,9 @@ implements OMService {
          em.persist( new Rate("R1_2", 10.2, 2, 1000, 1000000, ValidityLevel.C, begin, end));
          em.persist( new Rate("R1_1", 10.2, 1, 1000, 1000000, ValidityLevel.C, begin, end));
          
-         em.persist( new CreditContract("1", CreditContractStatus.proposal, new Integer(2), new Integer(2), null, new Integer(2), new Integer(2), "meineIBAN", "meineBIC", new ArrayList<Payment>(), test_rate_1, cust));
-         em.persist( new CreditContract("2", CreditContractStatus.revocation, new Integer(2), new Integer(2), null, new Integer(2), new Integer(2), "meineIBAN", "meineBIC", new ArrayList<Payment>(), test_rate_1, cust));
-         em.persist( new CreditContract("3", CreditContractStatus.disburse, new Integer(2), new Integer(2), null, new Integer(2), new Integer(2), "meineIBAN", "meineBIC", new ArrayList<Payment>(), test_rate_1, cust));
+         em.persist( new CreditContract("1", CreditContractStatus.proposal, new Integer(2), new Integer(2), new Date(), new Integer(2), new Integer(2), "meineIBAN", "meineBIC", new ArrayList<Payment>(), test_rate_1, cust));
+         em.persist( new CreditContract("2", CreditContractStatus.revocation, new Integer(2), new Integer(2), new Date(), new Integer(2), new Integer(2), "meineIBAN", "meineBIC", new ArrayList<Payment>(), test_rate_1, cust));
+         em.persist( new CreditContract("3", CreditContractStatus.disburse, new Integer(2), new Integer(2), new Date(), new Integer(2), new Integer(2), "meineIBAN", "meineBIC", new ArrayList<Payment>(), test_rate_1, cust));
          
          em.getTransaction().commit();
       }
